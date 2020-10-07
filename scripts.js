5614e0dc5012ae3ba96ea7bfe5f17ddb5c4c2c70
David-Hebert <king.david.abear@zoho.com>
2020-10-07 10:32:04 -0500

    initial commit

---
+++ scripts.js
@@ -0,0 +1,32 @@
+// We'll pre-populate this array with a couple objects just so it's not undefined if your internet connection isn't working properly.
+let arrayOfTodos = [
+    {
+    "userId": 14,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+  },
+  {
+    "userId": 20,
+    "id": 2,
+    "title": "delectus aut autem",
+    "completed": false
+  }]
+
+  const fetchTodos = () => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then( (response) => response.json())
+    .then( (json) => arrayOfTodos = json)
+  }
+
+  const logTodos = () => {
+    console.log(arrayOfTodos)
+  }
+
+  const populateTodos = () => {
+    let toDo = document.getElementById ("todo-list")
+    let newListElement = document.createElement ("li")
+    newListElement.innerHTML = arrayOfTodos [0].title
+    toDo.appendChild (newListElement)
+
+  };
