48599750ff9738cdc752433f6d1e12543d97a12d
David-Hebert <king.david.abear@zoho.com>
2020-10-12 14:50:32 -0500

    added filtering and styling

--- scripts.js
+++ scripts.js
@@ -28,10 +28,63 @@ let arrayOfTodos = [
  
     for (i = 0; i < arrayOfTodos.length; i++) {
         let newListElement = document.createElement ("li")
-        newListElement.innerHTML = arrayOfTodos [i].title
+        newListElement.innerHTML = arrayOfTodos[i].title
         toDo.appendChild (newListElement);
         if (arrayOfTodos[i].completed == false) {
             newListElement.classList.add("incomplete");
-        }        
+        } else {
+            newListElement.classList.add("complete");
+        }
     }
   };
+
+const findID = () => {
+  let toDos = document.getElementById('todo-list');
+  document.getElementById("todo-list").innerHTML = "";
+  toDos.innerHTML = "";
+  const myID = document.getElementById('user-id').value;
+  console.log(myID);
+  const arrayFilter = arrayOfTodos.filter(array => array.userId == myID);
+  for (let i = 0; i < arrayFilter.length; i++) {
+    let addLI = document.createElement('li');
+    addLI.innerHTML = arrayFilter[i].title;
+    toDos.appendChild(addLI);
+    console.log(arrayFilter[i].completed);
+    
+    if (arrayOfTodos[i].completed == false) {
+      addLI.classList.add('incomplete');
+      addLI.setAttribute("id", i);
+    } else {
+        addLI.classList.add('complete');
+    } 
+  }
+};
+
+const filterComplete = () => {
+  const x = document.querySelectorAll('.incomplete');
+  const y = document.querySelectorAll('.complete');
+  
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  for (i = 0; i < y.length; i++) {
+    y[i].style.display = "inline-block";
+  }
+  
+}
+
+
+
+
+const filterIncomplete = () => {
+  const x = document.querySelectorAll('.complete');
+  const y = document.querySelectorAll('.incomplete');
+
+
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  for (i = 0; i < y.length; i++) {
+    y[i].style.display = "inline-block";
+  }
+}
